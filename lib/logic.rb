465992a669d4eb0ef5ceeef54f974c156db4ed39
tmampa <tmampa94@gmail.com>
2021-06-02 00:35:49 +0200

    Link the webpage being scraped

--- lib/logic.rb
+++ lib/logic.rb
@@ -1,4 +1,13 @@
 require 'httparty'
 require 'nokogiri'
-require 'byebug'
-require 'json'
+
+class Scraper
+  attr_accessor :parse_page
+
+  def initialize
+    doc = HTTParty.get('http://www.nike.com/w/mens-nike-by-you-lifestyle-shoes-13jrmz6ealhznik1zy7ok')
+    @parse_page ||= Nokogiri::HTML(doc.body)
+  end
+
+
+end
