433367ee71c9802fe2a08fc3a588d4cd730bde91
tmampa <tmampa94@gmail.com>
2021-06-02 10:58:03 +0200

    Display for names and prices

--- bin/main.rb
+++ bin/main.rb
@@ -5,3 +5,8 @@ require_relative './lib/logic'
 scraper = Scraper.new
 names = scraper.p_names
 prices = scraper.p_prices
+
+(0...prices.size).each do |index|
+  puts "--- index: #{index + 1} ---"
+  puts "Name: #{names[index]} | Price: #{prices[index]}"
+end
